498b87cb5cbae82d76b74918614a215335ea9630
gioZAK <giovane.zakc@gmail.com>
2022-08-11 22:08:25 +0000

    Add function to calculate the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,55 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You Clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
-    }
+        });
+    };
+
+    runGame("addition");
+
 })
 
 /**
  * the main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkwon Game Type: ${gameType}`);
+        throw `Unkwon Game Type: ${gameType}. Aborting!`;
+    };
 }
 
 function checkAnswer() {
 
 }
 
+/**
+ * Get the operand1, operand2 and operator from the DOM 
+ * and execute the correct operation 
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator = "+") {
+        return operand1 + operand2
+    } else {
+        alert(`Unfineshed Operation: ${operator}`);
+        throw `Unfineshed Operation: ${operator}. Aborting!`;
+    }
 }
+
+
 
 function incrementScore() {
 
@@ -40,7 +65,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
